7a53e66b7b23ca2de3d76ef80640f6d0b538f13d
Mohammad Taqi <mohammad.taqi2420@gmail.com>
2025-09-20 14:16:48 -0400

    feat(brand-a): add BrandABulb using default integer file format

---
+++ src/main/java/com/assignment/dp/BrandABulb.java
@@ -0,0 +1,6 @@
+package com.assignment.dp;
+
+public class BrandABulb extends AbstractBulb {
+    public BrandABulb() { super("BrandA"); }
+    // Inherits default factory-method behavior (plain integer file)
+}
