1d3934c6063eb83843ca78d0aee40fac83c24fdd
Mohammad Taqi <mohammad.taqi2420@gmail.com>
2025-09-20 14:19:40 -0400

    feat(brand-a): add BrandALock using default integer file format

---
+++ src/main/java/com/assignment/dp/BrandALock.java
@@ -0,0 +1,6 @@
+package com.assignment.dp;
+
+public class BrandALock extends AbstractLock {
+    public BrandALock() { super("BrandA"); }
+    // Inherits default factory-method behavior (plain integer file)
+}
